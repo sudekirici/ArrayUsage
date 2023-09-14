5d739f2dd4b62fa83fce1d520ebc13ae3dd33eb5
sudekirici <sudeekiricii@gmail.com>
2023-09-14 14:40:47 +0300

    it is first commit about with array usage

---
+++ src/main/java/com/mycompany/arrayusage/ArrayUsage.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arrayusage;
+
+/**
+ *
+ * @author S
+ */
+public class ArrayUsage {
+
+    public static void main(String[] args) {
+        String languages[] = {"Java", "Python", "C"};
+        int level[] = {1,2,3,4,5};
+        System.out.println("Language : " + languages[0] + " Your level : " + level[4]);
+        System.out.println("Language : " + languages[1] + " Your level : " + level[3]);
+        System.out.println("Language : " + languages[2] + " Your level : " + level[1]);
+    }
+}
